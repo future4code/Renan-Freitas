16892e99d05a9c5b7c12035f723c6136459828fe
ReinEinzbern <renan.marinhofr@gmail.com>
2022-03-17 13:14:37 -0300

    Exercícios de Intro-Typescript

---
+++ modulo4/intro-typescript/src/exercicio5.ts
@@ -0,0 +1,17 @@
+function checaRenovacaoRG(anoAtual: number, anoNascimento: number, anoEmissao: number):boolean {
+ 
+    const idade = anoAtual - anoNascimento
+    const tempoCarteira = anoAtual - anoEmissao
+ 
+    const cond1 = idade <= 20 && tempoCarteira >= 5
+    const cond2 = idade > 20 && idade <= 50 && tempoCarteira >= 10
+    const cond3 = idade > 50 && tempoCarteira >= 15
+ 
+    return (cond1 || cond2 || cond3)
+ }
+
+const anoAtual = Number(process.argv[2])
+const anoNascimento = Number(process.argv[3])
+const anoEmissao = Number(process.argv[4])
+
+console.log(checaRenovacaoRG(anoAtual, anoNascimento, anoEmissao))
