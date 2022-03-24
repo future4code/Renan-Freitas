0fbae3496cb4befcc876502f62aa9621a30fdefb
Renan <renan.marinhofr@gmail.com>
2022-03-24 09:37:52 -0300

    Exercícios de Fluxo de dados no Backend

---
+++ modulo4/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,45 @@
+export const products = [
+    {
+        id: 7522121619,
+        name: 'iPhone 13 Pro 256GB Gray',
+        price: 7949
+    },
+    {
+        id: 8208704890,
+        name: 'iPhone 12 Pro 128GB Midnight Blue',
+        price: 5949
+    },
+    {
+        id: 8208718947,
+        name: 'iPhone 11 Pro 128GB Midnight Green',
+        price: 3949
+    }
+    ,
+    {
+        id: 2781903299,
+        name: 'Macbook Pro 14" M1 16GB 512GB SSD',
+        price: 16990
+    },
+    {
+        id: 3512922777,
+        name: 'Macbook Pro 16" M1 16GB 1024GB SSD',
+        price: 22750
+    }
+    ,
+    {
+        id: 6238914679,
+        name: 'Apple Watch Series 5 40mm',
+        price: 2100
+    },
+    {
+        id: 7297801655,
+        name: 'Apple Watch Series 6 40mm',
+        price: 2800
+    },
+    {
+        id: 9790850157,
+        name: 'Apple Watch Series 7 42mm',
+        price: 3300
+    }
+
+]
